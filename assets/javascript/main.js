cf6aa392261f4554a2610e3dfe344e6ec7f33793
rcowles3 <ryan.cowles3@gmail.com>
2017-05-20 11:59:04 -0700

    updated some css, linked js file to firebase db, and able to write to db and html with added employee

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -10,26 +10,48 @@
 
  firebase.initializeApp(config);
 
- // Click event to update html
+ // Variables to set type
+ var employee = "";
+ var role = "";
+ var startDate = 0;
+ var monthlyPay = 0;
+
+ // Variable to reference firebase db
+ var database = firebase.database();
+
+ // Click event to handle submit on app
  $('#addEmployeeBtn').click(function() {
 
      // PRevent form from running on own
      event.preventDefault();
 
      // Get input values
-     var employee = $('#emAdd').val().trim();
-     var role = $('#emRole').val().trim();
-     var startDate = $('#emStart').val().trim();
-     var monthlyPay = $('#emPay').val().trim();
+     employee = $('#emAdd').val().trim();
+     role = $('#emRole').val().trim();
+     startDate = $('#emStart').val().trim();
+     monthlyPay = $('#emPay').val().trim();
 
+     // Push values to html      
      $('#nameData').append(employee + "<br>");
      $('#roleData').append(role + "<br>");
      $('#startData').append(startDate + "<br>");
      $('#payData').append(monthlyPay + "<br>");
 
-     console.log(employee);
-     console.log(role);
-     console.log(startDate);
-     console.log(monthlyPay);
+     // Pushing data to firebase database
+     database.ref().push({
+         name: employee,
+         role: role,
+         startDate: startDate,
+         monthlyPay: monthlyPay,
+         // dateAdded: database.Servervalue.TIMESTAMP
+     });
 
+     // Firebase watcher, checking for added childs
+     database.ref().on("child-added", function(snapshot) {
+         // console.log(snapshot.val().employee);
+     });
+
+
+ }, function(errorObject) {
+     console.log("Errors handled: " + errorObject.code);
  });
